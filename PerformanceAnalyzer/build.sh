9699af97842c76013e22fb2782b4ef88f78f4a5d
NeXoS355 <>
2025-09-09 17:24:02 +0200

    changed build Parameter

--- PerformanceAnalyzer/build.sh
+++ PerformanceAnalyzer/build.sh
@@ -4,7 +4,7 @@
 echo "🚀 Kompiliere Go PerformanceAnalyzer..."
 
 # Binary für aktuelles System kompilieren
-go build -o perfAnalyzer .
+GOOS=linux GOARCH=amd64 GOAMD=v2 go build -o perfAnalyzer .
 
 if [ $? -eq 0 ]; then
   echo "✅ Erfolgreich kompiliert!"
